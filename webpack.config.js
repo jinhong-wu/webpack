ed3b95530c46002634d51a86d191f3ed0f1e9f21
jinhong-wu <jinhong212@163.com>
2020-09-21 21:18:46 +0800

    笔记本

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,16 @@ module.exports = {
         // (使用多个loader时，从右向左加载)
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["es2015"],
+          },
+        },
+      },
     ],
   },
 };
